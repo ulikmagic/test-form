dc26bebad8c7ffd10b984eda5be6aa60b8e85a72
ulikmagic <ulik.magic@gmail.com>
2024-02-27 18:10:53 +0600

    check useSWR

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { Button } from 'antd';
+import useSWR from 'swr';
+import { fetcher } from './utils/api';
+import { useEffect } from 'react';
+import { IUser } from './types/api';
+
 const App = () => {
+  const { data, error, isLoading } = useSWR<IUser[]>('https://jsonplaceholder.typicode.com/users', fetcher)
+
+  useEffect(() => console.log(data), [data])
   return (
     <div className="App">
-      initial app
+      {Array.isArray(data) && data.map(item =>
+        <div key={item.id}>
+          <p>{item.name}</p>
+          <p>{item.phone}</p>
+          <p>{item.email}</p>
+        </div>  
+      )}
     </div>
   );
 }
